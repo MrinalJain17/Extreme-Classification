c94c90132e5a36b75c98660fc940f562b0c551dd
Mrinal Jain <mrinaljain007@gmail.com>
2020-04-11 20:34:00 -0400

    Updates for validation data
    - Modified 'construct_data.py' to create validation set
    - Fixed 'split()' in 'utils.py'
    - Re-executed notebooks with training data

--- code/utils.py
+++ code/utils.py
@@ -2,15 +2,17 @@ import numpy as np
 from sklearn.metrics import label_ranking_average_precision_score
 
 
-def split(features, labels, test_split=0.1, random_state=42):
+def split(features, labels, test_split=0.2, random_state=42):
     """TODO"""
     rng = np.random.default_rng(random_state)
     num_examples = features.shape[0]
     num_valid = int(num_examples * test_split)
 
     # Randomly dividing between training and validation data
-    valid_index = rng.integers(0, num_examples, num_valid, endpoint=True)
-    train_index = np.array(list(set(range(num_examples)) - set(valid_index)))
+    index = np.arange(num_examples)
+    rng.shuffle(index)
+    valid_index = index[:num_valid]
+    train_index = index[num_valid:]
 
     train_features = features.loc[train_index].reset_index(drop=True)
     train_labels = labels.loc[train_index].reset_index(drop=True)
