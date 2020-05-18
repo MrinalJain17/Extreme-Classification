e1a7c6cdb0724dde6d64886c5fe6de8b5d127f52
Mrinal Jain <mrinaljain007@gmail.com>
2020-05-18 14:57:32 -0400

    Add code to convert test data into dense dataframe

--- code/construct_data.py
+++ code/construct_data.py
@@ -1,4 +1,14 @@
+"""
+Usage
+-----
+
+python construct_data.py
+python construct_data.py --test
+
+"""
+
 from pathlib import Path
+from argparse import ArgumentParser
 
 import numpy as np
 import pandas as pd
@@ -104,8 +114,50 @@ def construct(path, train=True):
     print(f"Saved dataframes.")
 
 
+def construct_test_data(path):
+    """TODO"""
+    # Default path for saving the dataframes
+    Path("../data/expanded").mkdir(parents=True, exist_ok=True)
+
+    # We don't need the column "ex_id" and "labels"
+    raw_data = pd.read_csv(f"{path}/test_no_label.csv", usecols=["features"])
+
+    # Formatting features
+    raw_data["features"] = (
+        raw_data["features"]
+        .str.split(" ")
+        .apply(lambda x: {int(i.split(":")[0]): float(i.split(":")[1]) for i in x})
+    )
+
+    raw_data["features"] = raw_data["features"].apply(
+        lambda x: [list(x.keys()), list(x.values())]
+    )
+
+    temp = np.zeros((raw_data.shape[0], NUM_FEATURES))
+    features = pd.DataFrame(temp)
+
+    def create_features(row):
+        features.loc[row.name, row["features"][0]] = row["features"][1]
+
+    # Creating features dataframe
+    print("Creating features...")
+    raw_data.progress_apply(lambda x: create_features(x), axis=1)
+
+    # Saving testing dataframe
+    features.to_csv(f"../data/expanded/test_features.csv", index=False, header=False)
+    print(f"Saved dataframe.")
+
+
 if __name__ == "__main__":
-    print("Constructing training data:")
-    construct("../data/raw")
-    print("Constructing development data:")
-    construct("../data/raw", train=False)
+    parser = ArgumentParser()
+    parser.add_argument("--test", action="store_true", default=False)
+    args = parser.parse_args()
+
+    if not args.test:
+        print("Constructing training data:")
+        construct("../data/raw")
+        print("Constructing development data:")
+        construct("../data/raw", train=False)
+    else:
+        print("Constructing testing data:")
+        construct_test_data("../data/raw")
